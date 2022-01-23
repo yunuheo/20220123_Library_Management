f6ce97d2472468c03a5ad239d83443a9f94c1dc4
허윤우 <hyoon516@naver.com>
2022-01-23 14:50:13 +0900

    Book 클래스의 멤버변수 및 메써드 추가
    - 책 정보 멤버변수
    - 책 정보 관련 메써드

--- src/codes/Book.java
+++ src/codes/Book.java
@@ -1,5 +1,24 @@
 package codes;
 
 public class Book {
+	
+	String title;
+	int rentFee;
+	int ageLimit;
+	User rentUser;
+	
+	 void showBookInfo() {
+		System.out.println("===== 도서 정보 출력 =====");
+		System.out.println("제목 : "+title);
+		System.out.println("대여료 : "+rentFee + "원");
+		
+//		연령제한이 0이면, 전체 이용가. 그 외에는 ?세 이용가
+		if (ageLimit==0) {
+			System.out.println("전체 이용가");
+		}
+		else {
+			System.out.println(ageLimit+"세 이용가");
+		}
+	}
 
 }
